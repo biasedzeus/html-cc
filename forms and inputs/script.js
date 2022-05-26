ec3f479f394d7f7cc7fa8698c06e5e8dc593572c
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-05-26 11:23:35 +0530

    form completed

--- forms and inputs/script.js
+++ forms and inputs/script.js
@@ -4,3 +4,9 @@ function togglePassword(){
     pass.type = "text":
     pass.type = "password"
 } 
+function handleSubmit(){
+    let fname = document.getElementById('firstName')
+    var fname2 = fname.innerText
+    console.log('First Name: ',fname.textContent)
+    console.log('First Name2: ',fname2)
+}
